542f93620f46798697d4abae26703511462c39e6
Costa <costa_torito@hotmail.com>
2020-11-02 10:30:58 +0100

    Imágenes galería definitvas

--- views/default/templates/template_galeria.php
+++ views/default/templates/template_galeria.php
@@ -9,47 +9,93 @@ echo '
 	<div class="contenedor-flex">
 
 		<div class="hijo-flex">
-		
 			<a class="fancybox" rel="group"
-    				href="./views/default/img/cocina.jpg" title="Terraza cubierta Merendalia"><img
-    				src="./views/default/img/cocina.jpg" class="foto img_ordenador"
-    				title="Terraza cubierta Merendalia" alt="Merendalia Celebración Fiesta Amigos Familia Juntarse" /></a>
+    				href="./views/default/img/cama_cojines_habitacion_decoracion_home_staging.png" title="Cama cojines habitación decoración Home Staging"><img
+    				src="./views/default/img/cama_cojines_habitacion_decoracion_home_staging.png" class="foto img_ordenador"
+    				title="Cama cojines habitación decoración Home Staging" alt="Cama cojines habitación decoración Home Staging" /></a>
     				<img
-    				src="./views/default/img/cocina.jpg" class="foto img_movil"
-    				title="Terraza cubierta Merendalia" alt="Merendalia Celebración Fiesta Amigos Familia Juntarse" />
-		
+    				src="./views/default/img/cama_cojines_habitacion_decoracion_home_staging.png" class="foto img_movil"
+    				title="Cama cojines habitación decoración Home Staging" alt="Cama cojines habitación decoración Home Staging" />
 		</div>
 
 		<div class="hijo-flex">
-			<img src="./views/default/img/cama_cojines.jpg" title="cama cojines"/>
+			<a class="fancybox" rel="group"
+    				href="./views/default/img/cocina_reformada_comedor_home_staging.png" title="Cocina reformada comedor Home Staging"><img
+    				src="./views/default/img/cocina_reformada_comedor_home_staging.png" class="foto img_ordenador"
+    				title="Cocina reformada comedor Home Staging" alt="Cocina reformada comedor Home Staging" /></a>
+    				<img
+    				src="./views/default/img/cocina_reformada_comedor_home_staging.png" class="foto img_movil"
+    				title="Cocina reformada comedor Home Staging" alt="Cocina reformada comedor Home Staging" />
 		</div>
 
 		<div class="hijo-flex">
-			<img src="./views/default/img/dormitorio.jpg" title="dormitorio"/>
+			<a class="fancybox" rel="group"
+    				href="./views/default/img/comedor_mesa_decoracion_home_staging.png" title="Comedor mesa decoración Home Staging"><img
+    				src="./views/default/img/comedor_mesa_decoracion_home_staging.png" class="foto img_ordenador"
+    				title="Comedor mesa decoración Home Staging" alt="Comedor mesa decoración Home Staging" /></a>
+    				<img
+    				src="./views/default/img/comedor_mesa_decoracion_home_staging.png" class="foto img_movil"
+    				title="Comedor mesa decoración Home Staging" alt="Comedor mesa decoración Home Staging" />
 		</div>
 
 		<div class="hijo-flex">
-			<img src="./views/default/img/salon_sofa.jpg" title="salón sofá"/>
+			<a class="fancybox" rel="group"
+    				href="./views/default/img/habitacion_adolescentes_reformada_home_staging.png" title="Habitación adolescentes reformada Home Staging"><img
+    				src="./views/default/img/habitacion_adolescentes_reformada_home_staging.png" class="foto img_ordenador"
+    				title="Habitación adolescentes reformada Home Staging" alt="Habitación adolescentes reformada Home Staging" /></a>
+    				<img
+    				src="./views/default/img/habitacion_adolescentes_reformada_home_staging.png" class="foto img_movil"
+    				title="Habitación adolescentes reformada Home Staging" alt="Habitación adolescentes reformada Home Staging" />
 		</div>
 
 		<div class="hijo-flex">
-			<img src="./views/default/img/sofa_cojines.jpg" title="sofá cojines"/>
+			<a class="fancybox" rel="group"
+    				href="./views/default/img/habitacion_cama_decoracion_plantas_home_staging.png" title="Habitación cama decoración plantas Home Staging"><img
+    				src="./views/default/img/habitacion_cama_decoracion_plantas_home_staging.png" class="foto img_ordenador"
+    				title="Habitación cama decoración plantas Home Staging" alt="Habitación cama decoración plantas Home Staging" /></a>
+    				<img
+    				src="./views/default/img/habitacion_cama_decoracion_plantas_home_staging.png" class="foto img_movil"
+    				title="Habitación cama decoración plantas Home Staging" alt="Habitación cama decoración plantas Home Staging" />
 		</div>
 
 		<div class="hijo-flex">
-			<img src="./views/default/img/cama_pale_planta.jpg" title="cama palé planta"/>
+			<a class="fancybox" rel="group"
+    				href="./views/default/img/habitacion_reformada_decoracion_home_staging.png" title="Habitación reformada decoración Home Staging"><img
+    				src="./views/default/img/habitacion_reformada_decoracion_home_staging.png" class="foto img_ordenador"
+    				title="Habitación reformada decoración Home Staging" alt="Habitación reformada decoración Home Staging" /></a>
+    				<img
+    				src="./views/default/img/habitacion_reformada_decoracion_home_staging.png" class="foto img_movil"
+    				title="Habitación reformada decoración Home Staging" alt="Habitación reformada decoración Home Staging" />
 		</div>
 
 		<div class="hijo-flex">
-			<img src="./views/default/img/habitacion_adolescente.jpg" title="habitación adolescente"/>
+			<a class="fancybox" rel="group"
+    				href="./views/default/img/pasillo_reformado_decoracion_home_staging.png" title="Pasillo reformado decoración Home Staging"><img
+    				src="./views/default/img/pasillo_reformado_decoracion_home_staging.png" class="foto img_ordenador"
+    				title="Pasillo reformado decoración Home Staging" alt="Pasillo reformado decoración Home Staging" /></a>
+    				<img
+    				src="./views/default/img/pasillo_reformado_decoracion_home_staging.png" class="foto img_movil"
+    				title="Pasillo reformado decoración Home Staging" alt="Pasillo reformado decoración Home Staging" />
 		</div>
 
 		<div class="hijo-flex">
-			<img src="./views/default/img/plantas_ventana.jpg" title="plantas ventana"/>
+			<a class="fancybox" rel="group"
+    				href="./views/default/img/sofa_plantas_decoracion_home_staging.png" title="Sofá plantas decoración Home Staging"><img
+    				src="./views/default/img/sofa_plantas_decoracion_home_staging.png" class="foto img_ordenador"
+    				title="Sofá plantas decoración Home Staging" alt="Sofá plantas decoración Home Staging" /></a>
+    				<img
+    				src="./views/default/img/sofa_plantas_decoracion_home_staging.png" class="foto img_movil"
+    				title="Sofá plantas decoración Home Staging" alt="Sofá plantas decoración Home Staging" />
 		</div>
 
 		<div class="hijo-flex">
-			<img src="./views/default/img/cocina.jpg" />
+			<a class="fancybox" rel="group"
+    				href="./views/default/img/ventana_decoracion_plantas_home_staging.png" title="Ventana decoración plantas Home Staging"><img
+    				src="./views/default/img/ventana_decoracion_plantas_home_staging.png" class="foto img_ordenador"
+    				title="Ventana decoración plantas Home Staging" alt="Ventana decoración plantas Home Staging" /></a>
+    				<img
+    				src="./views/default/img/ventana_decoracion_plantas_home_staging.png" class="foto img_movil"
+    				title="Ventana decoración plantas Home Staging" alt="Ventana decoración plantas Home Staging" />
 		</div>
 
 	</div>
